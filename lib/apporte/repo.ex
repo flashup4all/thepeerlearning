10a3662b004217e85e96757a9854542c491a70f1
Bardeson Lucky <flashup4all@gmail.com>
2023-08-05 17:42:22 +0100

    code clean up

--- lib/apporte/repo.ex
+++ lib/apporte/repo.ex
@@ -1,7 +1,19 @@
 defmodule PeerLearning.Repo do
+  require Logger
   use Ecto.Repo,
     otp_app: :peer_learning,
     adapter: Ecto.Adapters.Postgres
 
   use Scrivener, page_size: Application.compile_env!(:peer_learning, :default_pagination)
+
+  # def init(_type, config) do
+  #   database_url = System.get_env("DATABASE_URL")
+  #   if database_url == nil do
+  #     Logger.debug "$DATABASE_URL not set, using config"
+  #     {:ok, config}
+  #   else
+  #     Logger.debug "Configuring database using $DATABASE_URL"
+  #     {:ok, Keyword.put(config, :url, database_url)}
+  #   end
+  # end
 end
