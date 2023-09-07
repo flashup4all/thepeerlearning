13c8265ae9b96f5b228d43cc69c9d1290d1d3a68
Bardeson Lucky <flashup4all@gmail.com>
2023-09-07 04:05:24 +0100

    update profile

--- lib/apporte/accounts.ex
+++ lib/apporte/accounts.ex
@@ -9,6 +9,25 @@ defmodule PeerLearning.Accounts do
   alias PeerLearning.Accounts.{User, UserProfile, Children}
   alias PeerLearningWeb.Validators.{FilterChildren, AddChild, UpdateChild}
 
+  def update_user_profile(user_id, params) do
+    Repo.transaction(fn ->
+      with {:ok, %User{} = user} <- User.get_user(user_id),
+           {:ok, %UserProfile{} = user_profile} <- UserProfile.get_user_profile_by_user_id(user.id),
+           {:ok, %UserProfile{} = user_profile} <- UserProfile.update(user_profile, params) do
+
+            user_profile
+      else
+        {:error, error} ->
+          Repo.rollback(error)
+          {:error, error}
+
+        error ->
+          Repo.rollback(error)
+          {:error, error}
+      end
+    end)
+  end
+
   def list_children(%FilterChildren{page: _page, page_size: _page_size} = params) do
     Children.get_children(Map.from_struct(params))
   end
